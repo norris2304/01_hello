463f231893010f4127314078977278fbab22dbdd
norris2304 <norris2304@yahoo.com>
2022-04-26 13:23:02 -0500

    new

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
-# Purpose: Say hello
+"""
+Author: Jesse Norris <norris2304@yahoo.com>
+Purpose: Say hello
+"""
 
 import argparse
-def main():
+
+
+def get_args():
+    """Get the command-line arguments"""
+
     parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
-    args = parser.parse_args()
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
+    return parser.parse_args()
+
+
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
     print('Hello, ' + args.name + '!')
+
 
 if __name__ == '__main__':
     main()
-
-
-
